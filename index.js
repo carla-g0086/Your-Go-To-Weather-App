e726a033f99dfa2768e2727307a5642780d58cd5
carla-g0086 <carla.rutter1986@gmail.com>
2024-11-24 16:57:27 +0100

    added the time and date functions

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let descriptionElement = document.querySelector("#description");
 let feelsLikeElement = document.querySelector("#feelsLike");
+let timeElement=document.querySelector("#dateTime")
+let date=new Date(response.data.time*1000)
 
 cityElement.innerHTML = response.data.city;
 temperatureElement.innerHTML = Math.round(temperature);
@@ -13,9 +15,30 @@ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windElement.innerHTML = `${response.data.wind.speed}km/h`;
 descriptionElement.innerHTML = response.data.condition.description;
 feelsLikeElement.innerHTML = `${response.data.temperature.feels_like}&degC`;
+timeElement.innerHTML=formatDate(date);
 }
 
-
+function formatDate(date){
+   
+    let minutes=date.getMinutes();
+    let hours=date.getHours();
+    let days= [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    ];
+    let day=days[date.getDay()];
+    
+    if (minutes<10){
+        minutes=`0${minutes}`;
+    }
+return `${day}, ${hours}:${minutes}`;
+    
+}
   
 function search(event) {
   if (event) {
@@ -47,39 +70,4 @@ form.addEventListener("submit", search);
 
 search();
 
-let now = new Date();
-let h3 = document.querySelector("h3");
 
-let date = now.getDate();
-let hours = String(now.getHours()).padStart(2, "0");
-let minutes = String(now.getMinutes()).padStart(2, "0");
-let year = now.getFullYear();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-
-h3.innerHTML = `${day}, ${month} ${date}, ${year} 🔹 ${hours}:${minutes}`;
